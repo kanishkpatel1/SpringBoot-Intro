5ddce127be94cd26f493637ef30feefdbbd07682
Kanishk Patel <kanishkdeveshpatel@gmail.com>
2025-03-02 19:53:01 +0530

    Add files via upload

---
+++ Spring1/src/test/java/com/spring/uc1/helloworld/Spring1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spring.uc1.helloworld;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Spring1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
