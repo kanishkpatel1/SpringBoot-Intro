6126cdf2fda8f2526c78cc4e96e1fb53c563e78e
Kanishk Patel <kanishkdeveshpatel@gmail.com>
2025-03-02 19:44:47 +0530

    Add files via upload

---
+++ Spring1/Readme.md
@@ -0,0 +1,11 @@
+# HelloWorldSpringApp
+
+This is a simple Spring Boot application to display "Hello from BridgeLabz" using STS.
+
+## Steps to Run
+
+1. **Clone the repository** or create the project using Spring STS.
+2. **Ensure you have Java 17+ and Maven installed.**
+3. **Build the project:**
+   ```sh
+   mvn clean install
---
+++ Spring1/src/main/java/com/spring/uc1/helloworld/HelloWorldSpringApp.java
@@ -0,0 +1,12 @@
+package com.spring.uc1.helloworld;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HelloWorldSpringApp {
+	public static void main(String[] args) {
+		SpringApplication.run(HelloWorldSpringApp.class, args);
+	}
+}
+
